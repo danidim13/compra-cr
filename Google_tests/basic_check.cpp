0101c07e664ff4eb97a899b5ba6b092485e25275
Daniel <danidiaza@gmail.com>
2019-09-10 19:11:58 -0600

    Google Test

---
+++ Google_tests/basic_check.cpp
@@ -0,0 +1,14 @@
+//
+// Created by daniel on 10/09/19.
+//
+
+#include "gtest/gtest.h"
+
+TEST(basic_check, test_eq) {
+    EXPECT_EQ(1, 0);
+}
+
+TEST(basic_check, test_neq) {
+    EXPECT_NE(1, 0);
+}
+
